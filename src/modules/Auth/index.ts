b877df5ce39a98c073787c4eee71bbc893402ea2
Rizki Ardian <radeite8@gmail.com>
2023-09-22 09:21:55 +0700

    Add route validator

--- src/modules/Auth/index.ts
+++ src/modules/Auth/index.ts
@@ -1,10 +1,25 @@
+import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
+import { z } from 'zod';
 import { login, register } from './controller';
 
 const basePath = '/auth';
 
+const validator = zValidator(
+	'json',
+	z.object({
+		username: z.string(),
+		password: z.string(),
+	}),
+	(result, c) => {
+		if (!result.success) {
+			return c.json(result.error, 400);
+		}
+	},
+);
+
 const route = new Hono();
-route.post('/register', register);
-route.post('/login', login);
+route.post('/register', validator, register);
+route.post('/login', validator, login);
 
 export { basePath, route };
--- src/modules/Books/index.ts
+++ src/modules/Books/index.ts
@@ -1,13 +1,28 @@
+import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
+import { z } from 'zod';
 import { add, del, get, list, update } from './controller';
 
 const basePath = '/books';
 
+const validator = zValidator(
+	'json',
+	z.object({
+		name: z.string(),
+		author: z.string(),
+	}),
+	(result, c) => {
+		if (!result.success) {
+			return c.json(result.error, 400);
+		}
+	},
+);
+
 const route = new Hono();
 route.get('/', list);
 route.get('/:id', get);
-route.post('/', add);
-route.put('/:id', update);
+route.post('/', validator, add);
+route.put('/:id', validator, update);
 route.delete('/:id', del);
 
 export { basePath, route };
