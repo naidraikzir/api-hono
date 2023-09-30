9033688c341ad926870d5b1fa96ea9752de58247
Rizki Ardian <radeite8@gmail.com>
2023-10-01 00:36:40 +0700

    Add logger, cors, and serve uploaded files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { cors } from 'hono/cors';
+import { serveStatic } from 'hono/bun';
 import { routes } from './routes';
 
 const app = new Hono();
+app.use('*', logger());
+app.use('*', cors());
+app.use('/uploads/*', serveStatic({ root: './' }));
 app.route('/', routes);
 
 export default {
